43ee5eeb0e41a811353c4072f2fade7bf10336a5
anhminh310872 <anhminh310872@gmail.com>
2025-01-02 13:19:10 +0700

    Update allproduct.jsx

--- fanimation/src/allproduct.jsx
+++ fanimation/src/allproduct.jsx
@@ -68,7 +68,7 @@ function Products({ data }) {
     color: [],
     blade: [],
     brand: [],
-    priceRange: { min: "", max: "" }, // Default initialized price range object
+    priceRange: { min: "", max: "" },
   });
 
   const handleCheckboxChange = (field, value, isChecked) => {
@@ -76,14 +76,12 @@ function Products({ data }) {
       const newFilters = { ...prevFilters };
 
       if (isChecked) {
-        // For number-based filters like blade, use parseInt
         if (field === "blade") {
           newFilters[field] = [...newFilters[field], parseInt(value)];
         } else {
           newFilters[field] = [...newFilters[field], value];
         }
       } else {
-        // Remove value from the filters
         newFilters[field] = newFilters[field].filter((item) => item !== value);
       }
 
@@ -134,7 +132,6 @@ function Products({ data }) {
               });
             }
 
-            // If no specific conditions are met, return false
             return false;
           }
 
@@ -178,7 +175,7 @@ function Products({ data }) {
     console.log("Popup Data:", product);
   };
   const closePopup = () => {
-    setPopupData(null); // Đóng popup
+    setPopupData(null);
   };
 
   const ReviewSection = () => {
@@ -592,7 +589,7 @@ function Products({ data }) {
         )}
       </div>
           <div className="btncart">
-          <h3>Total: ${cartItems.reduce((total, item) => total + item.Price * item.quantity, 0)}</h3>
+          <h3>Total: ${cartItems.reduce((total, item) => Math.round(total + item.Price * item.quantity * 100.0) / 100.0, 0)}</h3>
             <button className="purchase">
               <strong>Purchase</strong>
             </button>
